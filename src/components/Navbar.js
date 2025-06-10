8dacb76ea936ab021c48e928e28681426b137496
James M Macapagal <jmmacapagal@businessolver.com>
2025-06-10 15:10:44 -0700

    061025 - navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './Navbar.css';
 
 function Navbar() {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const handleToggle = () => {
+        setIsExpanded(!isExpanded);
+    };
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow-sm">
+        <nav className={`navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow-sm ${isExpanded ? "show" : ""}`}>
             <div className="container">
                 <a className="navbar-brand" href="#hero">James Macapagal</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <button className="navbar-toggler" type="button" onClick={handleToggle}>
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className={`collapse navbar-collapse ${isExpanded ? "show animated" : ""}`} id="navbarNav">
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
                         <li className="nav-item"><a className="nav-link" href="#resume">Resume</a></li>
